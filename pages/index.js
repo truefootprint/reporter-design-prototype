c7877b916d864a8e4036f54089eec03fc1670c66
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-23 13:32:16 +0100

    Start styling the cards

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,36 @@ const Index = () => (
         </Summary>
 
         <Gallery>
-          <Card color="#12814f">first</Card>
-          <Card color="#fff120">second</Card>
-          <Card color="#a3c643">third</Card>
-          <Card color="#ff4c1f">fourth</Card>
-          <Card color="#c62b9f">fifth</Card>
+          <Card color="#12814f">
+            <span className="ordinal">1 of 5</span>
+            <h3>Clear the land</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+            <img src="/images/map.png" />
+          </Card>
+
+          <Card color="#fff120">
+            <span className="ordinal">2 of 5</span>
+            <h3>Lay the foundations</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+          </Card>
+
+          <Card color="#a3c643">
+            <span className="ordinal">3 of 5</span>
+            <h3>Build the walls</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+          </Card>
+
+          <Card color="#ff4c1f">
+            <span className="ordinal">4 of 5</span>
+            <h3>Put the roof on</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+          </Card>
+
+          <Card color="#c62b9f">
+            <span className="ordinal">5 of 5</span>
+            <h3>Educate the children</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+          </Card>
         </Gallery>
       </CardLayout>
     </Phone>
