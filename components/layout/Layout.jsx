8ae60a36ccebacb54e00e2ecdf27e24c919307f4
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-23 12:40:48 +0100

    Add a Summary component

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import css from "./styles.scss";
 
 import "./reset.scss";
 
 const Layout = ({ children }) => <>
+  <Head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i&display=swap" rel="stylesheet" />
+  </Head>
+
   <div className={css.layout}>
     {children}
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Layout from "../components/layout";
 import Phone from "../components/phone";
 import CardLayout from "../components/card_layout";
+import Summary from "../components/summary";
 import Gallery from "../components/gallery";
 import Card from "../components/card";
 
 const Index = () => (
   <Layout>
     <Phone>
-      <CardLayout title="Lay the foundations">
+      <CardLayout title="Build a school in Vihiga">
+        <Summary>
+          <h2>Project summary</h2>
+          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+          <button>Project contract</button>
+        </Summary>
+
         <Gallery>
           <Card>first</Card>
           <Card>second</Card>
