d50460e02fd9b86dab40f91289a94077934c7ba3
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-04 13:39:12 +0000

    Scroll to the top when changing view

--- components/card_layout/CardLayout.jsx
+++ components/card_layout/CardLayout.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useRef } from "react";
 import css from "./styles.scss";
 
-const CardLayout = ({ children, className }) => (
-  <div className={`${css.card_layout} ${className}`}>
-    <div className={css.content}>
-      {children}
+const CardLayout = ({ children, className }) => {
+  const ref = useRef();
+
+  useEffect(() => ref.current.scrollTo(0, 0), [children]);
+
+  return (
+    <div ref={ref} className={`${css.card_layout} ${className}`}>
+      <div className={css.content}>
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default CardLayout;
